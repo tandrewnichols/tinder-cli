b429be6261fd9c4e45515a81a416dd77b7606e44
Andrew Nichols <tandrewnichols@gmail.com>
2014-04-05 23:33:41 -0400

    Add some shared functions to utils

--- tinder.js
+++ tinder.js
@@ -7,14 +7,17 @@ var coercion = require('./lib/coercion');
 
 program
   .version(require('./package').version)
-  .usage('[options]')
-  .option('-f, --rc-file <path>', 'path to .tinderrc', '~/.tinderrc')
+  .usage('<command> <name> [options]')
+  .option('-o, --opts-file', 'path to tinder.opts', '~/tinder.json')
   .option('-d, --description', 'description of the project')
   .option('-a, --author', 'author name')
   .option('-e, --email', 'email of author')
-  .option('-k, --keywords <keyword', 'keywords for package.json', coercion.list, [])
-  .option('-r, --dependencies <module>', 'dependencies to be added to package.json', coercion.list, [])
-  .option('-v, --dev-dependencies <module>','devDependencies to be added to package.json', coercion.list, [])
+  .option('-g, --github-account', 'github account name')
+  .option('-k, --keywords [keyword]', 'keywords for package.json', coercion.list, [])
+  .option('-r, --dependencies [module]', 'dependencies to be added to package.json', coercion.list, [])
+  .option('-v, --dev-dependencies [module]','devDependencies to be added to package.json', coercion.list, [])
+  .option('-n, --no-extend', 'replace dependencies rather than extending')
+  .option('-m, --main', 'location of main file')
   .parse(process.argv);
 
 program.name = 'tinder';
