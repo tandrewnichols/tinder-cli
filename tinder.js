81b6b80235930eadc4ca8d87002b21643a0088a7
Andrew Nichols <tandrewnichols@gmail.com>
2014-04-21 07:24:49 -0400

    Add template option

--- tinder.js
+++ tinder.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+/*
+ * NOTE: process.execPath points to where node is. Use process.execPath.replace('/node', '') to get the global install location
+ */
+
 var program = require('commander'),
     cli = require('./lib/cli'),
     colors = require('colors'),
@@ -7,21 +11,19 @@ var program = require('commander'),
 
 program
   .version(require('./package').version)
-  .usage('<command> <name> [options]')
-  .option('-b, --base-dir <dir>', 'OS path or git url to base project', '~/tinder')
-  .option('-d, --description <description>', 'description of the project')
-  .option('-k, --keywords <keyword>', 'keywords for package.json', coercion.list, [])
-  .option('-i, --remove-deps <module>', 'Dependencies in base project to ignore', coercion.list, [])
-  .option('-n, --no-extend', 'replace dependencies rather than extending')
-  .option('-v, --vars <key>:<value>', 'Additional variables for template interpolation', coercion.obj, {})
-  .option('--no-clean', 'do not rm -rf on a faiure');
+  .usage('<command> <name> [options]');
 
 program.name = 'tinder';
 
 program
-  .command('new <name>')
+  .command('new <name> <template>')
   .alias('mk')
-  .description('initialize a new express app')
+  .description('Initialize new project <name> based on <template>')
+  .option('-d, --description <description>', 'description of the project')
+  .option('-k, --keywords <keyword>', 'keywords for package.json', coercion.list, [])
+  .option('-r, --remove-deps <module>', 'Dependencies in base project not to include', coercion.list, [])
+  .option('-v, --vars <key>:<value>', 'Additional variables for template interpolation', coercion.obj, {})
+  .option('--no-clean', 'do not rm -rf on a faiure')
   .action(cli.create);
 
 program.parse(process.argv);
