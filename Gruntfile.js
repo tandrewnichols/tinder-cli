533a1ed50394d020359a8312edc885f9717ea244
Andrew Nichols <tandrewnichols@gmail.com>
2014-07-24 08:29:16 -0400

    Move task alias to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,6 @@ var taskMaster = require('task-master');
 module.exports = function(grunt) {
   taskMaster(grunt);
 
+  grunt.registerTask('test', 'mochaTest');
   grunt.registerTask('default', ['test']);
 };
--- tasks/mochaTest.js
+++ tasks/mochaTest.js
@@ -1,14 +1,10 @@
-module.exports = function(grunt) {
-  grunt.registerTask('test', 'mochaTest');
-
-  return {
-    test: {
-      options: {
-        reporter: 'spec',
-        ui: 'mocha-given',
-        require: 'coffee-script/register'
-      },
-      src: ['test/**/*.coffee']
-    }
-  };
+module.exports = {
+  test: {
+    options: {
+      reporter: 'spec',
+      ui: 'mocha-given',
+      require: 'coffee-script/register'
+    },
+    src: ['test/**/*.coffee']
+  }
 };
